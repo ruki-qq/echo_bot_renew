83280a9b35bfa9fcb01a5b7ec0dd973c9115b0e9
ruki-qq <ruki@duck.com>
2024-04-01 18:00:37 +0300

    Add non-existing commands handling

--- echo_bot/handlers.py
+++ echo_bot/handlers.py
@@ -1,11 +1,16 @@
 from typing import Any
 
+from aiogram.enums import ContentType
 from aiogram.handlers import MessageHandler
 
 
 class ContentMsgHandler(MessageHandler):
     async def handle(self) -> Any:
         msg = self.event
+        if msg.content_type == ContentType.TEXT and msg.text.startswith("/"):
+            return msg.answer(
+                text="Wrong command.\nSupported commands:\n\n/start\n/help"
+            )
         try:
             ans = await msg.send_copy(chat_id=msg.chat.id)
         except TypeError:
